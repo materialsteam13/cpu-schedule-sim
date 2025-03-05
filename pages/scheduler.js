c6255b3cee305a8f879e246d3c80c53eb944e4fe
materialsteam13 <materialsteam13@gmail.com>
2025-03-04 22:36:16 -0600

    Bar Graph is shown on home screen, buttons don't change things yet

--- pages/scheduler.js
+++ pages/scheduler.js
@@ -1,11 +1,29 @@
-import dynamic from "next/dynamic";
-const ChartComponent = dynamic(() => import("../components/ChartComponent"), { ssr: false });
+import { useState } from 'react';
+import { fifoScheduler } from '../utils/fifo';
 
 export default function Scheduler() {
+    const [results, setResults] = useState([]);
+
+    const runFIFO = () => {
+        const processes = [
+            { id: 1, arrivalTime: 3, burstTime: 7 },
+            { id: 2, arrivalTime: 1, burstTime: 5 },
+            { id: 3, arrivalTime: 2, burstTime: 2 }
+        ];
+        setResults(fifoScheduler(processes));
+    };
+
     return (
         <div>
-            <h1>CPU Scheduling Visualization</h1>
-            <ChartComponent />
+            <h1>CPU Scheduling Simulator</h1>
+            <button onClick={runFIFO}>Run FIFO</button>
+            <ul>
+                {results.map((process, index) => (
+                    <li key={index}>
+                        Process {process.id}: Arrival Time {process.arrivalTime}, Burst Time {process.burstTime}
+                    </li>
+                ))}
+            </ul>
         </div>
     );
 }
